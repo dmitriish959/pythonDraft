ef3d63569f8ba52a032451d7708f63c99e2b5d47
dmitriish959 <80873437+dmitriish959@users.noreply.github.com>
2024-02-17 00:20:27 +0500

    Нужно поработать над ссылкой токена

--- 1_1/main.py
+++ 1_1/main.py
@@ -1,7 +1,11 @@
 import logging
 
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+#from telegram import Update, ForceReply
+#Старая версия
+#from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
+
 # ForceReply ?
 
 
@@ -10,7 +14,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
-logger = loging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 def start(engine: Update, context: CallbackContext) -> None:
@@ -18,26 +22,24 @@ def start(engine: Update, context: CallbackContext) -> None:
     user = engine.effective_user
     # Приветствие
     engine.message.reply_markdown_v2(
-        fr'Привет, {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
-    )
+        fr'Привет,{user.mention_markdown_v2()}!\',reply_markup=ForceReply(selective=True),')
 
-def halp_command(engine: Update, ontext: CallbackContext) -> None:
+def help_command(engine: Update, ontext: CallbackContext) -> None:
     engine.message.reply_text('Я сам себе помочь не могу, а ты еще тут клацаешь!')
 
-def eche(engine: Update, context: CallbackContext) -> None:
+def echo(engine: Update, context: CallbackContext) -> None:
     # Зеркалим запрос
     engine.message.reply_text(engine.message.text)
 
 def main() -> None:
     from .gitignore import token
-    car = Updater(token)
+    car = updater(token)
     dispatcher = car.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo ))
-    engine.start_polling()
-    engine.idle()
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__name__':
     main()
